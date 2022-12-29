0ef6b7bfc4385a1baebe9b2ed433c71b1ee9b69f
Ayman Rahman <ayman2002rahman@gmail.com>
2022-12-29 13:29:45 +0300

    Finished the parser for interpreting the string that the user will type in

--- main.js
+++ main.js
@@ -7,8 +7,190 @@
 import { Token } from '/Token.js'
 import { Vector } from './Vector.js'
 
+function isNumber(num) { //Checks to make sure that the character is a number
+    return num == '0' || num == '1' || num == '2' || num == '3' || num == '4' || num == '5' || num == '6' || num == '7' || num == '8' || num == '9'; 
+}
+
 function peek(stack) {
     return stack[stack.length - 1];
+}
+
+//Throw user errors if this string doesn't make sense (ex: '5si' is missing an 'n')
+function stringToTokenArray(equation) { //Takes in the user input string, reads it and converts it into an array of the respected tokens. The output array is a valid infix expression.
+    var array = [];
+    var i = 0; //iterator for the string
+    while(i < equation.length) { //Iterate through the entire equations string
+        if(i <= equation.length - 3) { //Make sure that there is enough characters left to represent a trig fuction name or log function 
+            if(equation.substring(i, i + 3) == "sin") {
+                array.push(new Token("sin"));
+                i += 3;
+            }
+            else if(equation.substring(i, i + 3) == "cos") {
+                array.push(new Token("cos"));
+                i += 3;
+            }
+            else if(equation.substring(i, i + 3) == "tan") {
+                array.push(new Token("tan"));
+                i += 3;
+            }
+            else if(equation.substring(i, i + 3) == "log") {
+                array.push(new Token("log"));
+                i += 3;
+            }
+            //Check for other 3 lettered word functions here
+        }
+        if(i <= equation.length - 2) {
+            if(equation.substring(i, i + 2) == "ln") {
+                array.push(new Token("ln"));
+                i += 2;
+            }
+        }
+
+        //Checking all operators
+        if(equation[i] == '+') {
+            array.push(new Token("+"));
+            i++;
+        }
+        else if(equation[i] == '-') {
+            array.push(new Token("-"));
+            i++;
+        }
+        else if(equation[i] == '*') {
+            array.push(new Token("*"));
+            i++;
+        }
+        else if(equation[i] == '/') {
+            array.push(new Token("/"));
+            i++;
+        }
+        else if(equation[i] == '^') {
+            array.push(new Token("^"));
+            i++;
+        }
+        
+        //Checking for parenthesees
+        else if(equation[i] == '(') {
+            if(array.length > 0) {
+                if(peek(array).getLexeme() == ')')
+                    array.push(new Token("*"));
+            }
+            array.push(new Token("("));
+            i++;
+        }
+        else if(equation[i] == ')') {
+            array.push(new Token(")"));
+            i++;
+        }
+
+        //Checks for a number
+        else if(isNumber(equation[i])) {
+            var k = i + 1;
+            while(k < equation.length) {//Checks if there are more numbers after to group up as one number represented in base 10
+                if(!isNumber(equation[k]))
+                    break;
+                k++;
+            }
+            var number = equation.substring(i, k); //From i to k including i but excluding k
+            array.push(new Token(number));
+            i += number.length;
+        }
+
+        else if(equation[i] == 'x') {
+            if(array.length > 0) {
+                if(peek(array).getType() == "number")
+                    array.push(new Token("*"));
+            }
+            array.push(new Token("x"));
+            i += 1;
+        }
+        
+        else
+            throw errorMessage; //Makes sure that the token array is only made up of valid tokens. The order however may be wrong.
+    }
+    return array;
+}
+
+function validateTokenArray(tokensInput) {//Checks the token array to make sure that is in the CFG. Throw user errors if its not.
+    var tokens = [];
+    for(let i = 0; i < tokensInput.length; i++)
+        tokens.push(new Token(tokensInput[i].getLexeme()));
+    var errorMessage = "Invalid math expression";
+
+    while(tokens.length > 0)
+        parseS();
+
+    function expectChar(c) {
+        let t = tokens.shift()
+        if(t.getLexeme() != c)
+            throw errorMessage;
+    }
+
+    function expectType(type) {
+        let t = tokens.shift();
+        if(t.getType() != type)
+            throw errorMessage;
+    }
+
+    function parseS() {
+        if(tokens.length == 0)
+            throw "Invalid";
+        let t = tokens[0];
+        if(t.getType() == "number") {
+            expectType("number");
+            if(tokens.length != 0) {
+                if(tokens[0].getType() == "operator")
+                    parseA();
+            }
+        }
+        else if(t.getLexeme() == '(') {
+            expectChar('(');
+            parseS();
+            expectChar(')');
+            if(tokens.length != 0) {
+                if(tokens[0].getType() == "operator")
+                    parseA();
+            }
+        }
+        else if(t.getType() == "function") {
+            expectType("function");
+            if(tokens[0].getType() == "number") {
+                expectType("number");
+                if(tokens.length != 0) {
+                    if(tokens[0].getType() == "operator")
+                        parseA();
+                }
+            }
+            else if(tokens[0].getLexeme() == '(') {
+                expectChar('(');
+                parseS();
+                expectChar(')');
+                if(tokens.length != 0) {
+                    if(tokens[0].getType() == "operator")
+                        parseA();
+                }
+            }
+            else
+                throw errorMessage;
+        }
+        else
+            throw errorMessage;
+    }
+
+    function parseA() {
+        expectType("operator");
+        parseS();
+        if(tokens.length != 0) {
+            if(tokens[0].getType() == "operator")
+                parseA();
+        }
+    }
+}
+
+function validateFunctionOfX(tokens) {
+    for(var i = 0; i < tokens.length; i++)
+        if(tokens[i].getLexeme() == "x")
+            return;
+    throw "Missing x";
 }
 
 function shuntingYardAlgorithm(tokens) {
@@ -110,6 +292,11 @@ function evaluateRPN(expression) { //expression is an array of tokens in RPN
             break;
     }
     evaluatedExpression.pop()
+    //Checking if we try to evaulate out of domain
+    if(value !== value) {
+        //Catch the domain error here
+    }
+
     evaluatedExpression.push(new Token(value.toString()))
     i++;
     
@@ -125,7 +312,8 @@ function evaluateRPN(expression) { //expression is an array of tokens in RPN
 //Be careful with inputs that are no tin the domain (need to catch that and throw some error in this function
 //Evaluates the equation at a specified x value
 //Ex: if the equation is 2/(x^2) then evaluating it at x = 3 returns 0.22222
-function evaluateEquation(expression, x) { // WE ARE ACCIDRNTLY CHANGING THE Xs PERMANENTLY
+//Pre-condition: expression is a valid math expression with x as the variable and in RPN notation, x is in the domain of the equation 
+function evaluateEquation(expression, x) {
     //Loops through the expression array and replaces the "x" tokens with the value inputed for x
     var replacedExpression = [];
     for(let i = 0; i < expression.length; i++)
@@ -143,7 +331,7 @@ function evaluateEquation(expression, x) { // WE ARE ACCIDRNTLY CHANGING THE Xs 
 //var equation = [new Token("sin"), new Token("x"), new Token("^"), new Token("2")];
 //document.body.innerHTML = '<h1>' + evaluateEquation(shuntingYardAlgorithm(equation), 3.1415) + '</h1>';
 
-function applyFunction(vector, pdaEquation) { //f is in not PDA notation
+function applyFunction(vector, pdaEquation) { //f is in RPN notation
     var appliedVector = [];
     for(var i = 0; i < vector.dimensions(); i++)
         appliedVector.push(evaluateEquation(pdaEquation, vector.getComponenet(i)));
@@ -156,7 +344,7 @@ function calculateLeadingCoefficient(x, y, f) {
     var xValues = new Vector(x);
     var yValues = new Vector(y);
     var appliedFunctionValues = applyFunction(xValues, pdaEquation);
-    return appliedFunctionValues.dotProduct(yValues) / appliedFunctionValues.magnitude() ** 2; //Formula is correct
+    return appliedFunctionValues.dotProduct(yValues) / appliedFunctionValues.magnitude() ** 2;
 }
 
 function calculateCoefficientOfDetermination(x, y, f) {
@@ -167,10 +355,18 @@ function calculateRegressionStats() {
 
 }
 
-var x = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
-var y = [3, 6, 2, 8, 16, 3, 7, 9, 12, 7];
-var equation = [new Token("x"), new Token("^"), new Token("3")];
+//Example of the prgram calculating the leading coefficient for any function and data points
+var x = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 5];
+var y = [3, 6, 2, 8, 16, 3, 7, 9, 12, 7, 2.5];
+var userInput = "log(x)";
 
-document.body.innerHTML = '<h1>' + calculateLeadingCoefficient(x, y, equation) + '</h1>';
-
+try {
+    var equation = stringToTokenArray(userInput);
+    validateTokenArray(equation);
+    validateFunctionOfX(equation);
+    document.body.innerHTML = '<h1>' + calculateLeadingCoefficient(x, y, equation) + '</h1>';
+}
+catch(err) {
+    document.body.innerHTML = '<h1>Invalid math expression</h1>';
+}
 
